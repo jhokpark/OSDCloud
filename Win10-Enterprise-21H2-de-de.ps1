0c26f0c40e9d01a3b54899d3d7becd89076d8108
jhokpark <109728212+jhokpark@users.noreply.github.com>
2022-07-21 11:31:20 +0200

    Update Win10-Enterprise-21H2-de-de.ps1

--- Win10-Enterprise-21H2-de-de.ps1
+++ Win10-Enterprise-21H2-de-de.ps1
@@ -4,9 +4,9 @@ Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
 
-if ((Get-MyComputerModel) -match ‘Virtual’) {
+if ((Get-MyComputerModel) -match 'Virtual') {
 
-Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”
+Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
 
 Set-DisRes 1600
 
@@ -14,23 +14,23 @@ Set-DisRes 1600
 
 #Make sure I have the latest OSD Content
 
-Write-Host -ForegroundColor Green “Updating OSD PowerShell Module”
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 
 Install-Module OSD -Force
 
-Write-Host -ForegroundColor Green “Importing OSD PowerShell Module”
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 
 Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 
-Write-Host -ForegroundColor Green “Start OSDCloud Kpark”
+Write-Host -ForegroundColor Green "Start OSDCloud Kpark"
 
 Start-OSDCloud -OSLanguage de-de -OSBuild 21H2 -OSEdition Enterprise -ZTI
 
 #Restart from WinPE
 
-Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 
 Start-Sleep -Seconds 20
 
