e140738d34f62d6c3264961c6e3435bb3a86d506
jhokpark <109728212+jhokpark@users.noreply.github.com>
2022-07-21 11:34:59 +0200

    Update Win10-Enterprise-21H2-de-de.ps1

--- Win10-Enterprise-21H2-de-de.ps1
+++ Win10-Enterprise-21H2-de-de.ps1
@@ -2,15 +2,6 @@ Write-Host -ForegroundColor Green “Starting OSDCloud Kpark ZTI”
 
 Start-Sleep -Seconds 5
 
-#Change Display Resolution for Virtual Machine
-
-if ((Get-MyComputerModel) -match 'Virtual') {
-
-Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
-
-Set-DisRes 1600
-
-}
 
 #Make sure I have the latest OSD Content
 
